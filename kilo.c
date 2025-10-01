f8eb53c3e9ca83c0c93707ef5e24ba166d7824da
emilio <emelgoza18@gmail.com>
2025-10-01 12:25:58 -0700

    set flags

--- kilo.c
+++ kilo.c
@@ -16,9 +16,10 @@ void enable_raw_mode (void) {
     atexit(disable_raw_mode);
 
     struct termios raw = orig_termios;
-
-    tcgetattr(STDIN_FILENO, &raw);
-    raw.c_lflag &= ~(ECHO | ICANON);
+    raw.c_iflag &= ~(BRKINT | ICRNL| INPCK | ISTRIP | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -29,9 +30,9 @@ int main(void) {
     char c;
     while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
         if (iscntrl(c)) {
-            printf("%d\n", c);
+            printf("%d\r\n", c);
         } else {
-            printf("%d ('%c')\n", c, c);
+            printf("%d ('%c')\r\n", c, c);
         }
     }
     return 0;
