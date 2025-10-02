e5f4ea09567601824e04a4096913d5493b9765aa
emilio <emelgoza18@gmail.com>
2025-10-02 15:59:04 -0700

    mapped all escape inputs

--- kilo.c
+++ kilo.c
@@ -15,7 +15,12 @@ enum editor_key {
     ARROW_LEFT = 1000,
     ARROW_RIGHT,
     ARROW_UP,
-    ARROW_DOWN
+    ARROW_DOWN,
+    DEL_KEY,
+    HOME_KEY,
+    END_KEY,
+    PAGE_UP,
+    PAGE_DOWN
 };
 
 
@@ -79,11 +84,34 @@ int editor_read_key(void) {
             return '\x1b';
 
         if (seq[0] == '[') {
+            if (seq[1] >= '0' && seq[1] <= '9') {
+                if (read(STDIN_FILENO, &seq[2], 1) != 1)
+                    return '\x1b';
+                if (seq[2] == '~') {
+                    switch (seq[1]) {
+                        case '1': return HOME_KEY;
+                        case '3': return DEL_KEY;
+                        case '4': return END_KEY;
+                        case '5': return PAGE_UP;
+                        case '6': return PAGE_DOWN;
+                        case '7': return HOME_KEY;
+                        case '8': return END_KEY;
+                    }
+                }
+            } else {
+                switch (seq[1]) {
+                    case 'A': return ARROW_UP;
+                    case 'B': return ARROW_DOWN;
+                    case 'C': return ARROW_RIGHT;
+                    case 'D': return ARROW_LEFT;
+                    case 'H': return HOME_KEY;
+                    case 'F': return END_KEY;
+                }
+            }
+        } else if (seq[0] == 'O') {
             switch (seq[1]) {
-                case 'A': return ARROW_UP;
-                case 'B': return ARROW_DOWN;
-                case 'C': return ARROW_RIGHT;
-                case 'D': return ARROW_LEFT;
+                case 'H': return HOME_KEY;
+                case 'F': return END_KEY;
             }
         }
         return '\x1b';
@@ -204,17 +232,25 @@ void editor_refresh_screen(void) {
 void editor_move_cursor(int key) {
   switch (key) {
     case ARROW_LEFT:
-      E.cx--;
-      break;
+        if (E.cx != 0) {
+            E.cx--;
+        }
+        break;
     case ARROW_RIGHT:
-      E.cx++;
-      break;
+        if (E.cx != E.screencols -1) {
+            E.cx++;
+        }
+        break;
     case ARROW_UP:
-      E.cy--;
-      break;
+        if (E.cy != 0) {
+            E.cy--;
+        }
+        break;
     case ARROW_DOWN:
-      E.cy++;
-      break;
+        if (E.cy != E.screenrows - 1) {
+            E.cy++;
+        }
+        break;
   }
 }
 
@@ -226,6 +262,22 @@ void editor_process_keypress(void) {
             write(STDOUT_FILENO, "\x1b[2J", 4);
             write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
+            break;
+
+        case HOME_KEY:
+            E.cx = 0;
+            break;
+        case END_KEY:
+            E.cx = E.screencols - 1;
+            break;
+        
+        case PAGE_UP:
+        case PAGE_DOWN:
+            {
+                int times = E.screenrows;
+                while (times--)
+                    editor_move_cursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+            }
             break;
 
         case ARROW_UP:
